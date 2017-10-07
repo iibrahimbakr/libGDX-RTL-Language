36b7936142a4249885b64dafba71e8d19615a2b7
Crowni <ibraheem44.ib@gmail.com>
2017-10-07 09:40:50 +0200

    initial

--- core/src/com/crowni/gdx/rtllang/arabic/ArUtils.java
+++ core/src/com/crowni/gdx/rtllang/arabic/ArUtils.java
@@ -19,16 +19,19 @@
 
 package com.crowni.gdx.rtllang.arabic;
 
+import com.badlogic.gdx.utils.CharArray;
+import com.crowni.gdx.rtllang.arabic.ArCharCode.*;
+
 /**
  * Created by Crowni on 10/5/2017.
  **/
 
 public class ArUtils {
     private static final char EMPTY = 0;
-    private static final ArCharCode.IndividualChar[] chars;
+    private static final IndividualChar[] chars;
 
     static {
-        chars = ArCharCode.IndividualChar.values();
+        chars = IndividualChar.values();
     }
 
     public static boolean isInvalidChar(char c) {
@@ -36,7 +39,7 @@ public class ArUtils {
     }
 
     public static int getCharType(char c) {
-        for (ArCharCode.IndividualChar individualChar : chars)
+        for (IndividualChar individualChar : chars)
             if (individualChar.getChar() == c) return individualChar.getType();
         return 0;
     }
@@ -47,19 +50,19 @@ public class ArUtils {
 
     public static char getStartChar(char c) {
         for (int i = 0; i < chars.length; i++)
-            if (chars[i].getChar() == c) return ArCharCode.StartChar.getChar(i);
+            if (chars[i].getChar() == c) return StartChar.getChar(i);
         return c;
     }
 
     public static char getCenterChar(char c) {
         for (int i = 0; i < chars.length; i++)
-            if (chars[i].getChar() == c) return ArCharCode.CenterChar.getChar(i);
+            if (chars[i].getChar() == c) return CenterChar.getChar(i);
         return c;
     }
 
     public static char getEndChar(char c) {
         for (int i = 0; i < chars.length; i++)
-            if (chars[i].getChar() == c) return ArCharCode.EndChar.getChar(i);
+            if (chars[i].getChar() == c) return EndChar.getChar(i);
         return c;
     }
 
@@ -67,31 +70,52 @@ public class ArUtils {
     public static char getLAM_ALF(char alf) {
         switch (alf) {
             case 1575:
-                return ArCharCode.IndividualChar.LAM_ALF.getChar();
+                return IndividualChar.LAM_ALF.getChar();
             case 1570:
-                return ArCharCode.IndividualChar.LAM_ALF_MAD.getChar();
+                return IndividualChar.LAM_ALF_MAD.getChar();
             case 1571:
-                return ArCharCode.IndividualChar.LAM_ALF_HAMZA_UP.getChar();
+                return IndividualChar.LAM_ALF_HAMZA_UP.getChar();
             case 1573:
-                return ArCharCode.IndividualChar.LAM_ALF_HAMZA_DOWN.getChar();
+                return IndividualChar.LAM_ALF_HAMZA_DOWN.getChar();
         }
         return alf;
     }
 
     public static boolean isALFChar(char c) {
         return
-                c == ArCharCode.IndividualChar.ALF.getChar()
-                        || c == ArCharCode.IndividualChar.ALF_HAMZA_UP.getChar()
-                        || c == ArCharCode.IndividualChar.ALF_HAMZA_DOWN.getChar()
-                        || c == ArCharCode.IndividualChar.ALF_MAD.getChar()
+                c == IndividualChar.ALF.getChar()
+                        || c == IndividualChar.ALF_HAMZA_UP.getChar()
+                        || c == IndividualChar.ALF_HAMZA_DOWN.getChar()
+                        || c == IndividualChar.ALF_MAD.getChar()
                 ;
     }
 
     public static boolean isLAMChar(char c) {
-        return c == ArCharCode.IndividualChar.LAM.getChar();
+        return c == IndividualChar.LAM.getChar();
     }
 
     public static boolean isComplexChar(ArGlyph glyph) {
         return glyph instanceof ArGlyphComplex;
+    }
+
+    public static CharArray getAllChars() {
+        CharArray array = new CharArray();
+        for (int i = 0; i < chars.length; i++)
+            if (!array.contains(IndividualChar.getChar(i)))
+                array.add(IndividualChar.getChar(i));
+
+        for (int i = 0; i < chars.length; i++)
+            if (!array.contains(StartChar.getChar(i)))
+                array.add(StartChar.getChar(i));
+
+        for (int i = 0; i < chars.length; i++)
+            if (!array.contains(CenterChar.getChar(i)))
+                array.add(CenterChar.getChar(i));
+
+        for (int i = 0; i < chars.length; i++)
+            if (!array.contains(EndChar.getChar(i)))
+                array.add(EndChar.getChar(i));
+
+        return array;
     }
 }
